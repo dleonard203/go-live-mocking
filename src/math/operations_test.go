d61e8cbaa272ad95abe3ed604847521028761809
Dave Leonard <david.leonard203@gmail.com>
2024-01-01 13:42:59 -0500

    add tests for Add

---
+++ src/math/operations_test.go
@@ -0,0 +1,38 @@
+package math
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAdd(t *testing.T) {
+	testCases := []struct {
+		name     string
+		nums     []int
+		expected int
+	}{
+		{
+			name:     "empty set adds up to 0",
+			nums:     []int{},
+			expected: 0,
+		},
+		{
+			name:     "all positive numbers",
+			nums:     []int{1, 2, 3},
+			expected: 6,
+		},
+		{
+			name:     "all negative numbers",
+			nums:     []int{-1, -2, -3},
+			expected: -6,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := Add(tc.nums...)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
